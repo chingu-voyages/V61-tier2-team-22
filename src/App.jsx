57c807284e0e3879f76727baa8128462371afa03
nahul100 <88331979+nahul100@users.noreply.github.com>
2026-06-22 22:45:09 +0600

    Implement secret word state and new game button
    Added state management for the secret word and a button to start a new game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import { getRandomWord } from "./utils/WordleUtils";
 
 function App() {
+  const [secretWord, setSecretWord] = useState(getRandomWord());
 
+  const startNewGame = () => {
+    setSecretWord(getRandomWord());
+  }
 
   return (
-    <></>
+    <>
+      <h1>Wordle</h1>
+      <button onClick={startNewGame}>
+        New Game
+      </button>
+
+      <h3>Secret Word: {secretWord}</h3>
+    </>
   )
 }
 
-export default App
+export default App;
