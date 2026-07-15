8aadebbc7badaa6c601491445ae35dd40c3a7c93
nahul100 <88331979+nahul100@users.noreply.github.com>
2026-07-15 16:27:57 +0600

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/V61-tier2-team-22/',
+  base: '/Wordle/',
   
 })
