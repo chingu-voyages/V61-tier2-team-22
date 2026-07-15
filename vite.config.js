7efcf0222b9488bb9fffaa8d0772357f7a1d1f75
nahul100 <88331979+nahul100@users.noreply.github.com>
2026-07-15 12:46:23 +0600

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/wordle/',
+  base: '/V61-tier2-team-22/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
