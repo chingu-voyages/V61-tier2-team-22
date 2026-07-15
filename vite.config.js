4886694af383ef187aa0673973c1c98516b5fc59
nahul100 <88331979+nahul100@users.noreply.github.com>
2026-07-15 18:16:40 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Wordle/',
+  base: '/V61-tier2-team-22/',
   
 })
