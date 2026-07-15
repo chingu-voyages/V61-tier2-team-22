72bbfb257383d782a21aa2e74fab54aa9a257335
nahul100 <88331979+nahul100@users.noreply.github.com>
2026-07-15 14:57:57 +0600

    Remove Tailwind CSS plugin from Vite config
    Removed Tailwind CSS plugin and path alias configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: '/V61-tier2-team-22/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  
+})
