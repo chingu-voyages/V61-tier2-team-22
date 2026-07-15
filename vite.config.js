83652463720dcd88014adaada2e37dd180202302
nahul100 <88331979+nahul100@users.noreply.github.com>
2026-07-15 13:45:21 +0600

    Remove outDir configuration from vite.config.js
    Removed build configuration for output directory.

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/V61-tier2-team-22/',
-  build: {
-    outDir: 'docs',
-  }
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
